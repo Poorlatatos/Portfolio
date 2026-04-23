f29e7e5f9a7927284988fcbf0ac0b780b0aea799
Polatatos <s10270392@connect.np.edu.sg>
2026-04-23 15:26:04 +0800

    Update 0.1

--- 3DModels/models.js
+++
@@ -1,5 +0,0 @@
-export const models = [
-    { file: '3DModels/vhs.glb', link: './project-pages/TheCost.html' },
-    { file: '3DModels/vhs.glb', link: 'page2.html' },
-    { file: '3DModels/vhs.glb', link: 'page3.html' }
-];
